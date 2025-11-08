63e2dbe23a0bf8c867592a8ae4346b99dc3c06c5
Кумова Ксения <k.kumova@vkteam.ru>
2025-11-08 19:14:08 +0300

    split text

--- bibliographic_backend/main.go
+++ bibliographic_backend/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	gigachatService "bibliographic_litriture_gigachat/gigachat"
 	search "bibliographic_litriture_gigachat/search"
+	"strings"
 
 	"encoding/json"
 	"log"
@@ -93,4 +94,8 @@ func main() {
 
 	log.Println("Server started on :8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
+}
+
+func splitText(text string) []string {
+	return strings.Split(text, "\n")
 }
