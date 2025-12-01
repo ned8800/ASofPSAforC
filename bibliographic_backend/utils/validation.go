8f66542239fc574a6762ac21013b530c1fdf74d7
sychnico <107313617+sychnico@users.noreply.github.com>
2025-12-01 19:41:35 +0300

    word count

--- bibliographic_backend/utils/validation.go
+++ bibliographic_backend/utils/validation.go
@@ -1,16 +1,50 @@
 package utils
 
+import (
+	"regexp"
+	"strings"
+)
+
 const minSearchSymbols = 5
 const minFormatSymbols = 15
+const minFormatWords = 4
 
 func SearchInputIsValid(input string) bool {
 
 	if len(input) < minSearchSymbols {
 		return false
 	}
+
+	if isDigitsOnly(input) {
+		return false
+	}
+
 	return true
 }
 
 func FormatInputIsValid(input string) bool {
-	return len(input) >= minFormatSymbols
+
+	if len(input) < minFormatSymbols {
+		return false
+	}
+
+	if countWords(input) < minFormatWords {
+		return false
+	}
+
+	if isDigitsOnly(input) {
+		return false
+	}
+
+	return true
+}
+
+func countWords(s string) int {
+	words := strings.Fields(s)
+	return len(words)
+}
+
+func isDigitsOnly(str string) bool {
+	match, _ := regexp.MatchString(`^\d+$`, str)
+	return match
 }
